d11c3a5961cf362ecdfccb28de070344a339a2a4
Dharmendra <131225409+dharmendra7500@users.noreply.github.com>
2023-06-17 10:11:44 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+  const navtoggle = document.getElementById('navtoggle');
+  const navbar = document.getElementById('navbar');
+  navtoggle.addEventListener('click', function run (){
+    navbar.classList.toggle('active');
+  });
